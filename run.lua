7b8cccbb49e583ce93378465ee43a35b057ecd3d
Ilya Radinsky <ilya_radinsky@hotmail.com>
2021-05-13 10:24:32 +0300

    Setup work_dir for tnt instance

--- run.lua
+++ run.lua
@@ -1,7 +1,26 @@
+#!/usr/bin/env tarantool
+
 require('strict').on()
+
+local log = require('log')
+local fio = require('fio')
+
+local WORKDIR = fio.abspath(fio.dirname(arg[0]))
+local DATADIR = WORKDIR .. '/' .. '.data'
+
+local ok, err = fio.mktree(DATADIR)
+if not ok then
+    error(err, 0)
+end
+
+log.info('\n')
+log.info('WORKDIR: ' .. WORKDIR)
+log.info('DATADIR: ' .. DATADIR)
+log.info('\n')
 
 box.cfg {
     listen = 3302,
+    work_dir = DATADIR,
 }
 
 local http_server = require('http.server')
@@ -10,14 +29,12 @@ local http_router = require('http.router')
 local HOST = '127.0.0.1'
 local PORT = 8089
 
-local function script_path() local fio = require "fio"; local b = debug.getinfo(2, "S").source:sub(2); local b_dir = fio.dirname(b); local lb = fio.readlink(b); while lb ~= nil do if not string.startswith(lb, '/') then lb = fio.abspath(fio.pathjoin(b_dir, lb)) end; b = lb; lb = fio.readlink(b) end return b:match("(.*/)") end
-
 local function on_index(req)
     return req:render({})
 end
 
 local server = http_server.new(HOST, PORT)
-local router = http_router.new({ app_dir = script_path() })
+local router = http_router.new({ app_dir = WORKDIR })
 server:set_router(router)
 router:route({ path = '/', method = 'GET', file = 'index.html' }, on_index)
 server:start()
