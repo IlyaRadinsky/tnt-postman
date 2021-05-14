a5b27ca6b507e959f7b68d03de627a0672bbcfaf
Ilya Radinsky <ilya_radinsky@hotmail.com>
2021-05-14 08:20:40 +0300

    Initial UI layout

--- public/static/app.js
+++ public/static/app.js
@@ -1,6 +1,27 @@
+function connect() {
+
+}
+
 webix.ui({
     rows: [
-        { view: "template", type: "header", template: "TNT Viewer" },
-        { view: "datatable", autoConfig: true, data: [ /*data array*/] }
-    ]
+        {
+            view: "toolbar",
+            padding: { right: 10, left: 10 },
+            elements: [
+                { view: "label", label: "TNT Viewer", width: 100 },
+                { view: "text", placeholder: "host:port", id: "host", width: 200 },
+                { view: "text", placeholder: "user", id: "user", width: 100 },
+                { view: "text", placeholder: "password", id: "password", width: 100 },
+                { view: "button", value: "Connect", css: "webix_primary", width: 100, click: connect },
+                {},
+                { view: "icon", icon: "wxi-close-circle warning" },
+            ]
+        },
+        {
+            cols: [
+                { view: "template", template: "Menu", width: 250 },
+                { view: "template", template: "Content", role: "placeholder" }
+            ]
+        },
+    ],
 });
